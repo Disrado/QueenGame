550aeb4dbb77fde730e7c080cf1445ebf6025d0c
Disrado <n.maltsev.dis@gmail.com>
2017-05-16 05:57:51 +0300

    Fixed button position and new size

--- sources/SceneManagment/Scenes/StartScene.cpp
+++ sources/SceneManagment/Scenes/StartScene.cpp
@@ -8,25 +8,25 @@ StartScene::StartScene(const sf::Vector2u& _windowSize, tgui::Gui *_gui, SceneMa
     tgui::Theme::Ptr theme = tgui::Theme::create("../GUITheme/Black.txt");
         
     playBtn = theme->load("Button");
-    playBtn->setSize(150, 50);
+    playBtn->setSize(200, 80);
     playBtn->setPosition((_windowSize.x / 2) - (playBtn->getSize().x / 2),
-                         (_windowSize.y / 2) - 80);
+                         (_windowSize.y / 2) - 100);
     playBtn->setText("Play");
     playBtn->connect("mousereleased",[_smgr](){ _smgr->replaceCurrentScene(Scenes::PrePlay); });
     gui->add(playBtn);
 
     settingsBtn = theme->load("Button");
-    settingsBtn->setSize(150, 50);
-    settingsBtn->setPosition((_windowSize.x / 2) - (playBtn->getSize().x / 2),
+    settingsBtn->setSize(200, 80);
+    settingsBtn->setPosition((_windowSize.x / 2) - (settingsBtn->getSize().x / 2),
                              (_windowSize.y / 2));
     settingsBtn->setText("Settings");
     settingsBtn->connect("mousereleased", [_smgr](){ _smgr->replaceCurrentScene(Scenes::Settings) ;});
     gui->add(settingsBtn);
 
     exitBtn = theme->load("Button");
-    exitBtn->setSize(150, 50);
-    exitBtn->setPosition((_windowSize.x / 2) - (playBtn->getSize().x / 2),
-                         (_windowSize.y / 2) + 80);
+    exitBtn->setSize(200, 80);
+    exitBtn->setPosition((_windowSize.x / 2) - (exitBtn->getSize().x / 2),
+                         (_windowSize.y / 2) + 100);
     exitBtn->setText("Exit");
     exitBtn->connect("mousereleased", [=](){ exit(0); });
     gui->add(exitBtn);
